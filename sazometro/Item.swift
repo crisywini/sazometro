d42d8f103a20a620e2bfbfbc812da9ed3e8ec6e6
Cristian Sánchez Pineda <harmaharcri@hotmail.com>
2026-02-20 12:39:47 -0500

    Initial Commit

---
+++ sazometro/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  sazometro
+//
+//  Created by Cristian Sánchez Pineda on 20/02/26.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
