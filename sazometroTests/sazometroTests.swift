dd6a3c8f2059056045d389dc9ddb392cf84d0f2e
Cristian Sánchez Pineda <harmaharcri@hotmail.com>
2026-02-21 18:06:45 -0500

    Organize project into MVVM architecture

---
+++ sazometroTests/sazometroTests.swift
@@ -0,0 +1,16 @@
+//
+//  sazometroTests.swift
+//  sazometroTests
+//
+//  Created by Cristian Sánchez Pineda on 21/02/26.
+//
+
+import Testing
+
+struct sazometroTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
